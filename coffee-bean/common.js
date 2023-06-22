84e8bdb911e1a0ae66d10100f5e49654dc872157
channy <ox043440@gmail.com>
2023-06-22 18:22:54 +0900

    230622

--- coffee-bean/common.js
+++ coffee-bean/common.js
@@ -7,6 +7,8 @@
         $(this).find('.depth2').stop().slideUp();
     });
 
+
+
     // prd-슬라이더
     $('.prd_list').slick({
         slidesToShow: 4,
@@ -29,3 +31,62 @@
     });
 });
 
+
+
+    // 검색 버튼 클릭 시 검색창 등장
+    // case1(기본)
+    // $(document).on("click", ".btn_search", function(){
+    //     if($(".search_box").hasClass("on")){            
+    //         $(".search_box").slideUp(500, function(){
+    //             $(".search_box").removeClass("on");
+    //         });
+    //     }else{
+    //         $(".search_box").slideDown(500, function(){
+    //             $(".search_box").addClass("on");
+    //         });
+    //     }
+    // });
+
+    // case2(변수사용)
+    // var abc = function(){
+    //     if($(".search_box").hasClass("on")){            
+    //         $(".search_box").slideUp(500, function(){
+    //             $(".search_box").removeClass("on");
+    //         });
+    //     }else{
+    //         $(".search_box").slideDown(500, function(){
+    //             $(".search_box").addClass("on");
+    //         });
+    //     }
+    // }
+
+    // $(document).on("click", ".btn_search", function(){
+    //     abc();
+    // });
+    
+    // case3(선기능구현 후 조건-함수사용)
+    var abc = function(opt){
+        if(opt=="open"){
+            $(".search_box").slideDown(500, function(){
+                $(".search_box").addClass("on");
+                $(".btn_search").addClass("test");
+            });
+        }
+        if(opt=="close"){
+            
+            $(".search_box").slideUp(500, function(){
+                $(".search_box").removeClass("on");
+                $(".btn_search").removeClass("test");
+            });
+        }
+    }
+
+    $(document).on("click", ".btn_search", function(){
+    
+        if($(".search_box").hasClass("on")){            
+            abc("close")
+        }else{
+            abc("open")
+        }
+    }
+    );
