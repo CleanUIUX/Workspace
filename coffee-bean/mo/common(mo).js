9f2761bc4e76c5b3437b94ed9e3774f9d07d6500
channy <ox043440@gmail.com>
2023-07-06 18:29:01 +0900

    230706

--- coffee-bean/mo/common(mo).js
+++ coffee-bean/mo/common(mo).js
@@ -9,4 +9,15 @@ $(document).ready(function(){
     speed: 1000,
     pauseOnHover: true,
   });
+
+  $('.news_list').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    dots: true,
+    arrows : false,
+    autoplaySpeed: 1000,
+    speed: 1000,
+    pauseOnHover: true,
+  });
 });
