191f2357301cca293068cb7798f418a696e1aa8f
Ro SenugChan <ox0434@finenuts.co.kr>
2023-09-10 14:58:08 +0900

    230910

---
+++ React 장인/my-app/src/pages/List.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const UserList = () => {
+    const users = [
+        {email: 'ryu@gamil.co.kr', name: '유재석'},
+        {email: 'kim@gamil.co.kr', name: '김종국'},
+        {email: 'haha@gamil.co.kr', name: '하하'},
+        {email: 'song@gamil.co.kr', name: '송지효'},
+    ];
+
+    return {
+        <table>
+            
+        </table>
+    }
+}
