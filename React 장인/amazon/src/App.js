72a94b41e718b0d96e41bed001ce62b95e5842f7
channy <ox043440@gmail.com>
2023-09-13 18:09:28 +0900

    230913

--- React 장인/amazon/src/App.js
+++ React 장인/amazon/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-import Header from './header';
+import Header from './Header';
+import Home from './Home';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Header />
+          <Home />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
